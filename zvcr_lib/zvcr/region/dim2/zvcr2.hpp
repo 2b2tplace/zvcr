a2a14c1f2a9f008cdff2b8c746bc18c8d291c567
crxyne <craynemail@gmail.com>
2025-09-18 16:11:59 +0200

    bump zvcr3 / zvcr2 file versions; add copy/move constructors

--- zvcr_lib/zvcr/region/dim2/zvcr2.hpp
+++ zvcr_lib/zvcr/region/dim2/zvcr2.hpp
@@ -11,10 +11,11 @@ namespace zvcr {
         ZVCR2_0_1_0_0,
         ZVCR2_0_1_1_0,
         ZVCR2_0_1_1_1,
-        ZVCR2_0_1_2_0
+        ZVCR2_0_1_2_0,
+        ZVCR2_0_1_3_0
     };
 
-    static constexpr auto ZVCR2_VER_LATEST = ZVCR2Version::ZVCR2_0_1_2_0;
+    static constexpr auto ZVCR2_VER_LATEST = ZVCR2Version::ZVCR2_0_1_3_0;
     static constexpr auto ZVCR2_FILE_PREFIX = "ZPRegion";
 
     struct ZVCR2File {
@@ -27,8 +28,15 @@ namespace zvcr {
             dimensionType(dimensionType),
             region(std::move(region)) {}
 
-        explicit ZVCR2File(const DimensionType dimensionType, const Region2d &region):
-            ZVCR2File(ZVCR2_VER_LATEST, dimensionType, region) {}
+        explicit ZVCR2File(const DimensionType dimensionType, Region2d region):
+            ZVCR2File(ZVCR2_VER_LATEST, dimensionType, std::move(region)) {}
+
+        ZVCR2File(ZVCR2File &&other) noexcept:
+            version(other.version),
+            dimensionType(other.dimensionType),
+            region(std::move(other.region)) {}
+
+        ZVCR2File(const ZVCR2File &other) = default;
     };
 
 }
--- zvcr_lib/zvcr/region/dim3/zvcr3.hpp
+++ zvcr_lib/zvcr/region/dim3/zvcr3.hpp
@@ -8,10 +8,11 @@ namespace zvcr {
     enum class ZVCR3Version {
         ZVCR3_0_0_0_1 = 1,
         ZVCR3_0_1_0_0,
-        ZVCR3_0_1_1_0
+        ZVCR3_0_1_1_0,
+        ZVCR3_0_1_2_0
     };
 
-    static constexpr auto ZVCR3_VER_LATEST = ZVCR3Version::ZVCR3_0_1_1_0;
+    static constexpr auto ZVCR3_VER_LATEST = ZVCR3Version::ZVCR3_0_1_2_0;
     static constexpr auto ZVCR3_FILE_PREFIX = "ZVRegion";
 
     struct ZVCR3File {
@@ -24,8 +25,15 @@ namespace zvcr {
             dimensionType(dimensionType),
             region(std::move(region)) {}
 
-        explicit ZVCR3File(const DimensionType dimensionType, const Region3d &region):
-            ZVCR3File(ZVCR3_VER_LATEST, dimensionType, region) {}
+        explicit ZVCR3File(const DimensionType dimensionType, Region3d region):
+            ZVCR3File(ZVCR3_VER_LATEST, dimensionType, std::move(region)) {}
+
+        ZVCR3File(ZVCR3File &&other) noexcept:
+            version(other.version),
+            dimensionType(other.dimensionType),
+            region(std::move(other.region)) {}
+
+        ZVCR3File(const ZVCR3File &other) = default;
     };
 
 }
