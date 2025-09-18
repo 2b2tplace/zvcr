7e1f8f8538d6cf04117a7b8b54d024df7e3e3011
crxyne <craynemail@gmail.com>
2025-09-18 16:18:49 +0200

    better test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <zvcr.hpp>
+#include <iostream>
+#include <chrono>
 
 int main() {
     uint16_t protocolVersion{};
@@ -9,14 +11,17 @@ int main() {
         return EXIT_FAILURE;
     }
     const auto t2 = std::chrono::high_resolution_clock::now();
+
+    const auto t3 = std::chrono::high_resolution_clock::now();
     const auto writeResult = zvcr::writeZVCRFile(file.value(), "r.-3.16.zvcr3.bak", protocolVersion);
     if (!writeResult.has_value()) {
         std::cerr << "Could not write file, write error = " << writeResult.error() << '\n';
         return EXIT_FAILURE;
     }
-    const auto t3 = std::chrono::high_resolution_clock::now();
+    const auto t4 = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Read took " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1) << ", write took " << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2) << '\n';
+    std::cout << "Read took " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    std::cout << ", write took " << std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3) << '\n';
     std::cout << "Wrote " << writeResult.value() << " bytes\n";
     return EXIT_SUCCESS;
 }
