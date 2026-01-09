c86dcc67e379b709a5f43b409f5825118db91f4e
crxyne <craynemail@gmail.com>
2026-01-09 21:59:47 +0100

    remove thread_pool.hpp, move to pluh

--- zvcr_lib/zvcr.hpp
+++ zvcr_lib/zvcr.hpp
@@ -3,7 +3,6 @@
 #include <zvcr/common/data_storage.hpp>
 #include <zvcr/region/file_location.hpp>
 #include <zvcr/common/generic_region.hpp>
-#include <zvcr/common/thread_pool.hpp>
 #include <zvcr/region/dim2/zvcr2.hpp>
 #include <zvcr/region/dim3/zvcr3.hpp>
 #include <zvcr/serialize/serialization.hpp>
